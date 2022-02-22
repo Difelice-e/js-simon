c05bffc01333c60eba3e7a972ff15524bc54e4f4
Difelice-e <difelice.emanuele@gmail.com>
2022-02-22 16:58:19 +0100

    esercizio completato + bonus

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// funzione numero random  
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -13,34 +14,53 @@ while (numeri.length < 5) {
         numeri.push(numero);
     }
 }
-// console.log(numeri);
-let numeriIndovinati = [];
 
 
+
+// recupero elementi html  
 const playArea = document.querySelector('.play-area');
 const playButton = document.querySelector('#play-button');
 const resultArea = document.querySelector('.result-area');
 const sendNumberButton = document.querySelector('#submit-user-number');
 
-playArea.innerHTML += `${numeri}`;
+playArea.innerHTML += `Memorizza questi numeri: ${numeri}`;
 
+
+
+// creazione array numeri inseriti dall'utente e numeri indovinati 
+let valoriUtente = [];
+let numeriIndovinati = [];
+
+// funzioni per chiedere numeri all'utente e controllare  
 function askUserNumber () {
     const inputNumeroUtente = document.querySelector('.input-area');
     inputNumeroUtente.style.display = 'inline-block';
 }
 
 function getUserNumber () {
-    const numeriUtente = document.querySelectorAll('.user-number').value;
-    console.log(numeriUtente); 
-    // qui il query selector mi da undefined  
+    let numeriUtente = document.querySelectorAll('.user-number');
+    for(let i = 0; i < numeriUtente.length; i++){
+        let newValue = numeriUtente[i].value;
+        valoriUtente.push(newValue);
+    }
+
+    console.log(valoriUtente); 
+    valoriUtente.forEach(element => {
+        if (numeri.includes(parseInt(element))) {
+            numeriIndovinati.push(element);
+        }
+    }) 
 
     resultArea.innerHTML = `Hai indovinato ${numeriIndovinati.length} numeri, tra i quali: ${numeriIndovinati}`;
 }
 
+
+// eventi al click 
 playButton.addEventListener('click', function() {
     playArea.style.display = 'none';
+    playButton.style.display = 'none';
 
-    const myTimeout = setTimeout(askUserNumber, 5000);
+    const myTimeout = setTimeout(askUserNumber, 30000);
 })
 
 sendNumberButton.addEventListener('click', function() {
