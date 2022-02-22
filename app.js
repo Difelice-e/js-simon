cb8c98a966c663f07afe1e36795dcfe10dcfcc71
Difelice-e <difelice.emanuele@gmail.com>
2022-02-22 16:33:18 +0100

    update esercizio

--- app.js
+++ app.js
@@ -17,19 +17,42 @@ while (numeri.length < 5) {
 let numeriIndovinati = [];
 
 
+const playArea = document.querySelector('.play-area');
+const playButton = document.querySelector('#play-button');
+const resultArea = document.querySelector('.result-area');
+const sendNumberButton = document.querySelector('#submit-user-number');
 
-alert(numeri)
+playArea.innerHTML += `${numeri}`;
 
-const myTimeout = setTimeout(getUserNumber, 5000);
+function askUserNumber () {
+    const inputNumeroUtente = document.querySelector('.input-area');
+    inputNumeroUtente.style.display = 'inline-block';
+}
 
 function getUserNumber () {
-    for (i = 0; i < 5; i++) {
-        const numeroUtente = parseInt(prompt("inserisci un numero"));
-        if (numeri.includes(numeroUtente)) {
-            numeriIndovinati.push(numeroUtente);
-        }
-    }
+    const numeriUtente = document.querySelectorAll('.user-number').value;
+    console.log(numeriUtente); 
+    // qui il query selector mi da undefined  
+
+    resultArea.innerHTML = `Hai indovinato ${numeriIndovinati.length} numeri, tra i quali: ${numeriIndovinati}`;
 }
+
+playButton.addEventListener('click', function() {
+    playArea.style.display = 'none';
+
+    const myTimeout = setTimeout(askUserNumber, 5000);
+})
+
+sendNumberButton.addEventListener('click', function() {
+    getUserNumber ();
+})
+
+
+
+
+
+
+
 
 
 
